5ce12b759d4aa9d2cd46d8ff652cae09d9f6c8a1
EdisonLopezEC <edisonlopezec@gmail.com>
2023-07-02 22:47:46 -0500

    testing test solutions

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          importante
+          Learn React importante
         </a>
       </header>
     </div>
