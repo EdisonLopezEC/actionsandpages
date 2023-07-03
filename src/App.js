6fc428d18e0ea97095d366e831702dc02aa6437a
Mariela Amanta <mamanta4438@uta.edu.ec>
2023-07-02 23:17:18 -0500

    Cambios de Mariela

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React importante
+          Cambios realizados por Mariela 
         </a>
       </header>
     </div>
